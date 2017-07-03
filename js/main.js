83a339b191a665125419ae34f5f2041e73690f77
iamblocksberg <blocksberg.dw@gmail.com>
2017-07-03 12:02:03 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<title>Title</title>
+	<!-- Social tags -->
+	<meta name="keywords" content="responsive bootstrap template, bootstrap ui kit, responsive ui kit">
+	<meta name="description" content="Start your development with a beautiful Bootstrap 4 UI kit. It is yours Free.">
+	<!-- Schema.org markup for Google+ -->
+	<meta itemprop="name" content="UI Kit">
+	<meta itemprop="description" content="Start your development">
+	<meta itemprop="image" content="http://s3.amazonaws.com/image.jpg">
+	<!-- Twitter Card data -->
+	<meta name="twitter:card" content="product">
+	<meta name="twitter:site" content="@creativetim">
+	<meta name="twitter:title" content="UI Kit">
+	<meta name="twitter:description" content="Start your development">
+	<meta name="twitter:creator" content="@usernameeeeeee">
+	<meta name="twitter:image" content="http://s3.amazonaws.com/image.jpg">
+	<meta name="twitter:datal" content="UI Kit">
+	<meta name="twitter:labell" content="Product Type">
+	<meta name="twitter:data2" content="Free">
+	<meta name="twitter:label2" content="Price">
+	<!-- Open Graph data -->
+	<meta property="fb:app_id" content="0000000000000">
+	<meta property="og:title" content="UI Kit">
+	<meta property="og:type" content="article">
+	<meta property="og:url" content="http://ui-kit/index.html">
+	<meta property="og:image" content="http://s3.amazonaws.com/image.jpg">
+	<meta property="og:description" content="Start your development"> 
+	<meta property="og:site_name" content="Awesome">
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<!-- Script -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+
+</body>
+</html>
